8cce2b06c43a14323b2f5af2b6a8d97ad0b8b98b
JiJiBo <38462693+JiJiBo@users.noreply.github.com>
2024-12-01 11:27:27 +0800

    init

--- start_train.sh
+++ start_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # 使用nohup将 train_stylegan.py.py放到后台运行，并将输出保存
-nohup python3 train_stylegan.py.py > log.txt 2>&1 &
+nohup python3 train_stylegan.py > log.txt 2>&1 &
 echo "训练脚本已在后台启动，输出保存在log.txt"
